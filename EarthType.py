58a53e5a54c8410b5ceeb400e2f93449004dad8d
alixunderscore <alix.platier-favier@laplateforme.io>
2023-03-31 02:05:42 +0200

    Update 2.0 - Not finished

--- EarthType.py
+++
@@ -1,8 +0,0 @@
-from Pokemon import Pokemon
-
-class Earth(Pokemon):
-    def __init__(self, name, lvl, pa):
-        super().__init__(name, lvl, pa)
-
-    def __str__(self):
-        return f'Name: {self.__name},\nPV: {self.__pv},\n Attack: {self.pa},\n Defense: {self.defense}'
